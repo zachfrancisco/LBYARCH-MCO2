5ed585af9e6aaf9d15a49e121d1a3eda1bbf50c6
zachfrancisco <zachary_francisco@dlsu.edu.ph>
2025-11-23 17:01:34 +0800

    bug fix

--- function.asm
+++ function.asm
@@ -1,13 +1,17 @@
 section .data
-msg db 'asm works', 0
+    msg db 'asm works', 0
+    float_255 dq 255.0
 
 section .text
 bits 64
 default rel
-global imgCvtGrayDoubltToInt
+global imgCvtGrayDoubleToInt
 extern printf
 
-imgCvtGrayDoubltToInt:
+; RCX = matrix_ptr (double*)
+; RDX = rows (int)
+; R8  = cols (int)
+imgCvtGrayDoubleToInt:
     sub rsp, 8*5
     mov rcx, msg
     call printf
