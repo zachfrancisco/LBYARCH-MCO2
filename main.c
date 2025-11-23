8a38a0486fe5eb048ee8c21a7e81c8fe09bfea08
zachfrancisco <zachary_francisco@dlsu.edu.ph>
2025-11-24 01:38:27 +0800

    added time, readme, 10x10

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 extern void imgCvtGrayDoubleToInt(double *matrix_ptr, int *dest_ptr, int rows, int cols);
 
 int main()
 {
+    clock_t start_time = clock();
+
     int rows = 0;
     int cols = 0;
     double matrix[500][500] = {0};
+    double time = 0.0;
 
     scanf("%d", &rows);
     scanf("%d", &cols);
@@ -28,7 +32,7 @@ int main()
     {
         for (int j = 0; j < cols; j++)
         {
-            printf("%.2lf ", matrix[i][j]);
+            printf("%5.2lf ", matrix[i][j]);
         }
         printf("\n");
     }
@@ -40,12 +44,19 @@ int main()
     {
         for (int j = 0; j < cols; j++)
         {
-            printf("%d ", int_matrix[i * cols + j]);
+            printf("%3d", int_matrix[i * cols + j]);
+            if (j < cols - 1)
+                printf(", ");
         }
         printf("\n");
     }
 
     free(int_matrix);
+
+    clock_t end_time = clock();
+    time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    printf("\nTime: %.3f seconds", time);
 
     return 0;
 }
