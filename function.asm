f89b425d022720628b5d0bb51fa98dbaad13c6f2
Richard Limtin <jeremylimtin@gmail.com>
2025-11-23 21:38:20 +0800

    feat: computation

--- function.asm
+++ function.asm
@@ -1,7 +1,7 @@
 section .data
-    msg db 'asm works', 0
     float_255 dq 255.0
-
+    fmt_val db "%d, ", 0
+    fmt_nl db 10, 0
 section .text
 bits 64
 default rel
@@ -11,10 +11,58 @@ extern printf
 ; RCX = matrix_ptr (double*)
 ; RDX = rows (int)
 ; R8  = cols (int)
-imgCvtGrayDoubleToInt:
-    sub rsp, 8*5
-    mov rcx, msg
-    call printf
-    add rsp, 8*5
+imgCvtGrayDoubleToInt:  
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
     
-    ret
+    sub rsp, 8*5
+    
+    mov rbx, rcx
+    mov r12, rdx
+    mov r13, r8
+    
+    xor r14, r14
+    
+    OuterLoop:
+        cmp r14, r12
+        jge End
+        
+        xor r15, r15
+    
+    InnerLoop:
+        cmp r15, r13
+        jge EndInner
+        
+        mov rax, r14
+        imul rax, 500
+        add rax, r15
+        
+        movsd xmm0, [rbx + rax * 8]
+        mulsd xmm0, [float_255]
+        cvttsd2si rdx, xmm0
+        
+        lea rcx, [fmt_val]
+        call printf
+        
+        inc r15
+        jmp InnerLoop
+
+      EndInner:
+        lea rcx, [fmt_nl]
+        call printf
+        
+        inc r14
+        jmp OuterLoop
+        
+       End:
+        add rsp, 40
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop rbx
+        
+        ret
--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-extern void imgCvtGrayDoubleToInt();
+extern void imgCvtGrayDoubleToInt(double *matrix_ptr, int rows, int cols);
 
-int main() {
+int main()
+{
     int rows = 0;
     int cols = 0;
     double matrix[500][500] = {0};
@@ -11,19 +12,23 @@ int main() {
     scanf("%d", &cols);
     printf("rows = %d\ncols = %d\n", rows, cols);
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
             scanf("%lf", &matrix[i][j]);
         }
     }
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
             printf("%.2lf ", matrix[i][j]);
         }
         printf("\n");
     }
 
-    imgCvtGrayDoubleToInt((double*)matrix, rows, cols);
+    imgCvtGrayDoubleToInt((double *)matrix, rows, cols);
     return 0;
 }
