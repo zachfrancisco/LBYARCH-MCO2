dcd8401f1a8d9f60f2c24020fb02e673cc81050a
zachfrancisco <zachary_francisco@dlsu.edu.ph>
2025-11-24 18:46:09 +0800

    bug fix for time

--- main.c
+++ main.c
@@ -10,10 +10,7 @@ int main()
     int cols = 0;
     int *int_matrix;
     double matrix[500][500] = {0};
-    double time = 0.0;
-    double total_time = 0.0;
-    int temp = 0;
-    double c_time = 0.0;
+    double nasm_total_time = 0.0;
     double c_total_time = 0.0;
 
     // Read input
@@ -42,10 +39,35 @@ int main()
         printf("\n");
     }
 
+    //C only
+    clock_t start_time = clock();
+    for (int z = 0; z < 30; z++)
+    {
+        printf("\n----------------------------------------");
+        printf("\nIteration %d:\n", z + 1);
+
+        printf("\nConverted integer matrix:\n");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                int_matrix[i * cols + j] = (int)(matrix[i][j] * 255);
+                printf("%3d", int_matrix[i * cols + j]);
+                if (j < cols - 1)
+                    printf(", ");
+            }
+            printf("\n");
+        }
+    }
+    clock_t end_time = clock();
+    c_total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("\nC Total Time: %.3f seconds", c_total_time);
+
+    //NASM call
+    start_time = clock();
+
     for (int k = 0; k < 30; k++)
     {
-        clock_t start_time = clock();
-
         printf("\n----------------------------------------");
         printf("\nIteration %d:\n", k + 1);
         imgCvtGrayDoubleToInt((double *)matrix, int_matrix, rows, cols);
@@ -61,50 +83,19 @@ int main()
             }
             printf("\n");
         }
-
-        clock_t end_time = clock();
-        time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-        printf("\nTime: %.3f seconds", time);
-
-        total_time += time;
     }
+    end_time = clock();
+    nasm_total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    printf("\nNasm Total Time: %.3f seconds", nasm_total_time);
 
-    for (int z = 0; z < 30; z++)
-    {
-        clock_t start_time = clock();
-
-        printf("\n----------------------------------------");
-        printf("\nIteration %d:\n", z + 1);
-
-        printf("\nConverted integer matrix:\n");
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                temp = matrix[i][j] * 255;
-                printf("%3d", temp);
-                if (j < cols - 1)
-                    printf(", ");
-            }
-            printf("\n");
-        }
-
-        clock_t end_time = clock();
-        c_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-        printf("\nTime: %.3f seconds", c_time);
-
-        c_total_time += c_time;
-    }
+    // Print final outputs
+    printf("\n----------------------------------------");
+    printf("\n\nAverage Time C with Nasm: %.3f seconds\n", nasm_total_time / 30.0);
+    printf("Total Time C with Nasm: %.3f seconds\n", nasm_total_time);
 
     printf("\n----------------------------------------");
     printf("\n\nAverage Time C ONLY: %.3f seconds\n", c_total_time / 30.0);
     printf("Total Time C ONLY: %.3f seconds\n", c_total_time);
-
-    printf("\n----------------------------------------");
-    printf("\n\nAverage Time C with Nasm: %.3f seconds\n", total_time / 30.0);
-    printf("Total Time C with Nasm: %.3f seconds\n", total_time);
-    printf("\n----------------------------------------");
 
     return 0;
 }
